f9328e7e870ffcb3cd358fd6adedffe7a1e61af8
Swarup_Pattnaik <pattnaik.swarup@gmail.com>
2016-09-27 09:34:33 +0530

    Added dependency from JSQMessageController

--- FacebookMessage/AppDelegate.swift
+++ FacebookMessage/AppDelegate.swift
@@ -11,49 +11,112 @@ import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
-
+    
     var window: UIWindow?
-
-
+    var localNotification : UILocalNotification!
+    var controller : MasterViewController!
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         let splitViewController = self.window!.rootViewController as! UISplitViewController
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
         navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem()
         splitViewController.delegate = self
-
+        
+        self.registerForLocalNotifications(application)
+        
         let masterNavigationController = splitViewController.viewControllers[0] as! UINavigationController
-        let controller = masterNavigationController.topViewController as! MasterViewController
+        controller = masterNavigationController.topViewController as! MasterViewController
         controller.managedObjectContext = self.managedObjectContext
         return true
     }
-
+    
+    // MARK: Register for push
+    func registerForLocalNotifications(application: UIApplication) {
+        let viewAction = UIMutableUserNotificationAction()
+        viewAction.identifier = "VIEW_IDENTIFIER"
+        viewAction.title = "View"
+        viewAction.activationMode = .Foreground
+        
+        let newsCategory = UIMutableUserNotificationCategory()
+        newsCategory.identifier = "NEWS_CATEGORY"
+        newsCategory.setActions([viewAction], forContext: .Default)
+        
+        let categories: Set<UIUserNotificationCategory> = [newsCategory]
+        
+        let notificationSettings = UIUserNotificationSettings(forTypes: [.Badge, .Sound, .Alert], categories: categories)
+        application.registerUserNotificationSettings(notificationSettings)
+    }
+    
+    
+    func application(application: UIApplication, didRegisterUserNotificationSettings notificationSettings: UIUserNotificationSettings) {
+        if notificationSettings.types != .None {
+            
+            localNotification = UILocalNotification()
+            localNotification.soundName = "Default"
+        }
+    }
+    
+    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification)
+    {
+        print(notification)
+        guard notification.userInfo != nil else {
+            return
+        }
+        let fromName = notification.userInfo!["fromName"] as? String
+        let toName   = notification.userInfo!["toName"] as? String
+        let text = notification.userInfo!["body"] as? String
+        let isSent = notification.userInfo!["isSent"] as? Bool
+        
+        let dateFormat = NSDateFormatter()
+        dateFormat.dateFormat = "dd/MM/yyyy HH:mm:ss a"
+        let date = dateFormat.stringFromDate(NSDate())
+        
+        let messageData : [String:AnyObject] = ["fromName":fromName!, "text":text!, "toName":toName!, "timeStamp": date, "isSent" : isSent!]
+        
+        
+        controller.insertNewObject(messageData)
+        
+        NSNotificationCenter.defaultCenter().postNotificationName("UpdateDetailViewNotification", object: self, userInfo:["messageData": messageData])
+        
+        
+    }
+    
+    // could not check on simulator
+    func application(application: UIApplication, handleActionWithIdentifier identifier: String?, forLocalNotification notification: UILocalNotification, completionHandler: () -> Void) {
+        
+        print(notification)
+        print(identifier)
+        
+        
+        // 3
+        if identifier == "VIEW_IDENTIFIER" {
+            
+        }
+        
+    }
+    
+    
     func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        let oldNotes = application.scheduledLocalNotifications
+        if oldNotes?.count > 0 {
+            application.cancelAllLocalNotifications()
+        }
     }
-
+    
     func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
     }
-
+    
     // MARK: - Split view
-
+    
     func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool {
         guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
         guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
@@ -64,24 +127,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         return false
     }
     // MARK: - Core Data stack
-
+    
     lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.SP.FacebookMessage" in the application's documents Application Support directory.
         let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         return urls[urls.count-1]
     }()
-
+    
     lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = NSBundle.mainBundle().URLForResource("FacebookMessage", withExtension: "momd")!
+        
+        let modelURL = NSBundle.mainBundle().URLForResource("HitMe", withExtension: "momd")!
         return NSManagedObjectModel(contentsOfURL: modelURL)!
     }()
-
+    
     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
         // Create the coordinator and store
         let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("SingleViewCoreData.sqlite")
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("MessagesStore.sqlite")
+        print(url)
         var failureReason = "There was an error creating or loading the application's saved data."
         do {
             try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
@@ -90,41 +152,36 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
             var dict = [String: AnyObject]()
             dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
             dict[NSLocalizedFailureReasonErrorKey] = failureReason
-
+            
             dict[NSUnderlyingErrorKey] = error as NSError
             let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
             abort()
         }
         
         return coordinator
     }()
-
+    
     lazy var managedObjectContext: NSManagedObjectContext = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
         let coordinator = self.persistentStoreCoordinator
         var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
         managedObjectContext.persistentStoreCoordinator = coordinator
         return managedObjectContext
     }()
-
+    
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         if managedObjectContext.hasChanges {
             do {
                 try managedObjectContext.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
                 NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
                 abort()
             }
         }
     }
-
+    
 }
 
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+source 'https://github.com/MachieLal/JSQMessagesViewController.git'
+platform :ios, '9.3'
+target 'FacebookMessage' do
+
+pod 'JSQMessagesViewController', :path => './'
+
+end
